983ea96aeced854e071013bb2b78e8539ec91e44
Carlos Ayuso <carlos.ayuso@gracia.lasalle.cat>
2025-07-03 21:45:29 +0200

    feat: add new llm and add timing logs
    Changed system prompt source to 'system_prompt2.txt' and updated the model to 'llama3:latest'. Added timing and status logs to the /generate endpoint for better monitoring of response generation performance.

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import time
+
 from fastapi import FastAPI, Body
 from fastapi.params import Query
 from pydantic import BaseModel
@@ -7,32 +9,42 @@ import ollama
 # To serve within all the network interfaces available run: uvicorn main:app --reload --host 0.0.0.0 --port 8000
 
 
-
 app = FastAPI()
+
 
 class PromptRequest(BaseModel):
     prompt: str
 
 
-with open('system_prompt.txt', 'r', encoding='utf-8') as file:
+with open('system_prompt2.txt', 'r', encoding='utf-8') as file:
     system_prompt = file.read()
 
-print (f"System prompt loaded: {system_prompt[:50]}...")  # Print first 50 characters for verification
+print(f"System prompt loaded: {system_prompt[:50]}...")  # Print first 50 characters for verification
+
+
 @app.post("/generate")
 def generate(request: PromptRequest = Query(...)):
+    inicio = time.perf_counter()
 
     """
     Generate a response based on the provided prompt.
     """
     print(f"Received prompt: {request.prompt}")
+    print("Generating response...")
     try:
+
         response = ollama.chat(
-            model="dolphin-mistral",
+            model="llama3:latest",
             messages=[
                 {"role": "system", "content": system_prompt},
                 {"role": "user", "content": request.prompt}]
         )
+        print("Response generated successfully.")
         return {"response": response["message"]["content"]}
 
     except Exception as e:
         return {"error": str(e)}
+
+    finally:
+        fin = time.perf_counter()
+        print(f"Response generated in {fin - inicio:.2f} seconds")
